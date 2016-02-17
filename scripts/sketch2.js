2ad97262ae8bafa21b2dd006a2785c50b9c5189d
Sam Madhu <94samyu@gmail.com>
2016-02-17 17:06:55 -0500

    added

--- scripts/sketch2.js
+++ scripts/sketch2.js
@@ -5,11 +5,11 @@ function setup() {
 }
 
 function draw() {
-  background(204,70,200);
+  background(20,20,20);
   var dirY = (mouseY / height - 0.5) * 3;
   var dirX = (mouseX / width - 0.5) * 3;
-  directionalLight(100, 100, 20, dirX, dirY, 1);
-  ambientLight(204,70,200);
+  directionalLight(150, 40, 150, dirX, dirY, 1);
+  ambientLight(20,20,20);
   translate(0,0,0);
   sphere(height/2,50);
 
