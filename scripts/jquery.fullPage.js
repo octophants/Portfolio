e7f9120f5f33a4011a0667e2e43a5a770ca6ec61
Sam Madhu <94samyu@gmail.com>
2016-02-18 03:00:15 -0500

    added

--- scripts/jquery.fullPage.js
+++ scripts/jquery.fullPage.js
@@ -116,7 +116,7 @@
             css3: true,
             scrollingSpeed: 700,
             autoScrolling: true,
-            fitToSection: true,
+            fitToSection: false,
             fitToSectionDelay: 1000,
             easing: 'easeInOutCubic',
             easingcss3: 'ease',
--- scripts/sketch2.js
+++ scripts/sketch2.js
@@ -8,10 +8,10 @@ function draw() {
   background(20,20,20);
   var dirY = (mouseY / height - 0.5) * 3;
   var dirX = (mouseX / width - 0.5) * 3;
-  directionalLight(150, 40, 150, dirX, dirY, 1);
+  directionalLight(204, 40, 200, dirX, dirY, 1);
   ambientLight(20,20,20);
   translate(0,0,0);
-  sphere(height/2,50);
+  sphere(height/1.8,50);
 
 }
 
